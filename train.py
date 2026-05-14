0c9bf5e74f8c7a245a04302d30a96869bbdb8e12
Region_of_Luck_11 <19102663204@163.com>
2026-05-14 23:03:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -110,24 +110,73 @@ def train_model(config=None):
             # logits = logits.view(-1, logits.size(-1))
             # tgt_output = tgt_output.view(-1)
 
+            loss = loss_fn(
+                logits.reshape(-1, logits.size(-1)),
+                tgt_output.reshape(-1)
+            )
+
             optimizer.zero_grad()
-            loss = loss_fn(logits.view(-1, logits.size(-1)), tgt_output.view(-1))
+
             loss.backward()
-            lr_scheduler.step()  ###############
+
+            torch.nn.utils.clip_grad_norm_(
+                model.parameters(),
+                max_norm=1.0
+            )
+
             optimizer.step()
-            losses += loss.item()
+
+            lr_scheduler.step()
+
+            total_loss += loss.item()
+
             # logits:[tgt_len, batch_size, tgt_vocab_size]  tgt_output:[tgt_len,batch_size]
-            acc, _, _ = accuracy(logits, tgt_output, data_loader.PAD_IDX)
-            msg = f'Epoch:{epoch}, Batch:[{idx}/{len(train_iter)}], Train loss: {loss.item():.3f}, Train acc: {acc:.3f}'
+            acc, _, _ = accuracy(
+                logits,
+                tgt_output,
+                data_loader.PAD_IDX
+            )
+
+            total_acc += acc
+
+            msg = (
+                f'Epoch:{epoch}, '
+                f'Batch:[{idx}/{len(train_iter)}], '
+                f'Train loss: {loss.item():.3f}, '
+                f'Train acc: {acc:.3f}'
+            )
+
             logging.info(msg)
-        train_loss = losses / len(train_iter)
-        msg = f'Epoch:{epoch},  Train loss: {train_loss:.3f}'
+
+        train_loss = total_loss / len(train_iter)
+        train_acc = total_acc / len(train_iter)
+
+        msg = (
+            f'Epoch:{epoch}, '
+            f'Train loss: {train_loss:.3f}, '
+            f'Train acc: {train_acc:.3f}'
+        )
+
         logging.info(msg)
-        if epoch % 2 == 0:
-            acc = evaluate(config, val_iter, translation_model, data_loader)
-            logging.info(f"Accuracy on Validation set: {acc:}")
-            state_dict = deepcopy(translation_model.state_dict())
+
+        val_acc = evaluate(
+            config,
+            val_iter,
+            model,
+            data_loader
+        )
+
+        logging.info(f'Accuracy on Validation set: {val_acc:.3f}')
+
+        if val_acc > best_val_acc:
+
+            best_val_acc = val_acc
+
+            state_dict = deepcopy(model.state_dict())
+
             torch.save(state_dict, model_save_path)
+
+            logging.info(f'Save best model to {model_save_path}')
 
 
 def evaluate(config, val_iter, model, data_loader):
